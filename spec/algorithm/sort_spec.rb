a3e9d1396ae43e8f55aff26d24dc1efd39933918
jason <644458812@qq.com>
2013-09-07 10:50:22 +0800

    refactory the rspec test group to shared_examples_for

--- spec/algorithm/sort_spec.rb
+++ spec/algorithm/sort_spec.rb
@@ -14,46 +14,46 @@ describe Sort do
     Sort.new.send(sort, to_sort).should == sorted
   end
 
-
-  it "# select sort" do
-    assert_equal :select_sort, @to_sort_n, sorted_n
-    assert_equal :select_sort, @to_sort_c, sorted_c
+  shared_examples_for "sort for:" do |sort|
+    it "1 element" do
+      assert_equal sort, [1], [1]
+      assert_equal sort, ['a'], ['a']
+    end
+    it "2 element" do
+      assert_equal sort, [2, 1], [1, 2]
+      assert_equal sort, ['b', 'a'], ['a', 'b']
+      assert_equal sort, [2, 2], [2, 2]
+    end
+    it "3 element" do
+      assert_equal sort, [2, 3, 1], [1, 2, 3]
+      assert_equal sort, ['b', 'c', 'a'], ['a', 'b', 'c']
+      assert_equal sort, [2, 1, 2], [1, 2, 2]
+    end
+    it "4 element" do
+      assert_equal sort, [4, 2, 3, 1], [1, 2, 3, 4]
+      assert_equal sort, ['d', 'b', 'c', 'a'], ['a', 'b', 'c', 'd']
+      assert_equal sort, [3, 2, 1, 2], [1, 2, 2, 3]
+      assert_equal sort, [2, 2, 1, 2], [1, 2, 2, 2]
+    end
+    it ">4 element" do
+      assert_equal sort, [4, 2, 5, 3, 1], [1, 2, 3, 4, 5]
+      assert_equal sort, ['e', 'd', 'b', 'c', 'a'], ['a', 'b', 'c', 'd', 'e']
+      assert_equal sort, [3, 2, 4, 1, 2], [1, 2, 2, 3, 4]
+      assert_equal sort, [2, 2, 1, 3, 2], [1, 2, 2, 2, 3]
+      assert_equal sort, [4, 2, 5, 3, 6, 1], [1, 2, 3, 4, 5, 6]
+    end
   end
 
-  it "# bubble sort" do
-    assert_equal :bubble_sort, @to_sort_n, sorted_n
-    assert_equal :bubble_sort, @to_sort_c, sorted_c
+  context "#select sort" do
+    it_behaves_like 'sort for:', :select_sort
+  end
+
+  context "#bubble sort" do
+    it_behaves_like 'sort for:', :bubble_sort
   end
 
   context "#quick sort" do
-    it "1 element" do
-      assert_equal :quick_sort, [1], [1]
-      assert_equal :quick_sort, ['a'], ['a']
-    end
-    it "2 element" do
-      assert_equal :quick_sort, [2, 1], [1, 2]
-      assert_equal :quick_sort, ['b', 'a'], ['a', 'b']
-      assert_equal :quick_sort, [2, 2], [2, 2]
-    end
-    it "3 element" do
-      assert_equal :quick_sort, [2, 3, 1], [1, 2, 3]
-      assert_equal :quick_sort, ['b', 'c', 'a'], ['a', 'b', 'c']
-      assert_equal :quick_sort, [2, 1, 2], [1, 2, 2]
-    end
-    it "4 element" do
-      assert_equal :quick_sort, [4, 2, 3, 1], [1, 2, 3, 4]
-      assert_equal :quick_sort, ['d', 'b', 'c', 'a'], ['a', 'b', 'c', 'd']
-      assert_equal :quick_sort, [3, 2, 1, 2], [1, 2, 2, 3]
-      assert_equal :quick_sort, [2, 2, 1, 2], [1, 2, 2, 2]
-    end
-    it ">4 element" do
-      assert_equal :quick_sort, [4, 2, 5, 3, 1], [1, 2, 3, 4, 5]
-      assert_equal :quick_sort, ['e', 'd', 'b', 'c', 'a'], ['a', 'b', 'c', 'd', 'e']
-      assert_equal :quick_sort, [3, 2, 4, 1, 2], [1, 2, 2, 3, 4]
-      assert_equal :quick_sort, [2, 2, 1, 3, 2], [1, 2, 2, 2, 3]
-      assert_equal :quick_sort, [4, 2, 5, 3, 6, 1], [1, 2, 3, 4, 5, 6]
-    end
-
+    it_behaves_like 'sort for:', :quick_sort
   end
 
 
